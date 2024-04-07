c8ae8f2ef9fbf3c2e8fbb3bff7c015c27645d238
Imossy <madsj0304@gmail.com>
2024-04-07 15:47:28 -0300

    Att Creche #feita alterações no código e adicionado o banco de dados.

--- todos/views.py
+++ todos/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
+from django.views.generic import ListView, CreateView, UpdateView
+from django.urls import reverse_lazy
+from .models import Todo
 
-# listar as tarefas
+class TodoListView(ListView):
+    model = Todo
 
-def todo_list(request):
-    return render(request, "todos/todo_list.html")
+class TodoCreateView(CreateView):
+    model = Todo
+    fields = ["nome_aluno", "nome_mae", "cpf_mae", "nome_pai", "cpf_pai", "endereco", "telefone", "dt_inicio", "mensalidade", "dt_saida"]
+    success_url = reverse_lazy ("todo_list")
+
+class TodoUpdateView(UpdateView):
+    model = Todo
+    fields = ["nome_aluno", "nome_mae", "cpf_mae", "nome_pai", "cpf_pai", "endereco", "telefone", "dt_inicio", "mensalidade", "dt_saida"]
+    success_url = reverse_lazy ("todo_list")
